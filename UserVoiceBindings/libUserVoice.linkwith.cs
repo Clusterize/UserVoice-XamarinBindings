7ff2a3237a6a189ac37b8f720c8d5ae5de1ba38b
Nikola Irinchev <irinchev@me.com>
2015-07-09 01:40:28 +0300

    Update to UserVoice 3.2.3

--- UserVoiceBindings/libUserVoice.linkwith.cs
+++ UserVoiceBindings/libUserVoice.linkwith.cs
@@ -1,4 +1,3 @@
-using System;
 using ObjCRuntime;
 
 [assembly: LinkWith ("libUserVoice.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64, ForceLoad = true)]

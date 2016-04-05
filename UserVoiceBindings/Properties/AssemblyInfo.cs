7c225b14f4c9e569f86e64de265d6b6fb99c52b6
Nikola Irinchev <irinchev@me.com>
2016-04-05 12:21:20 +0300

    add smartlink

---
+++ UserVoiceBindings/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using Foundation;
+
+[assembly: LinkerSafe]
+[assembly: AssemblyTitle("UserVoiceBindings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("nikolairinchev")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.*")]
--- UserVoiceBindings/libUserVoice.linkwith.cs
+++ UserVoiceBindings/libUserVoice.linkwith.cs
@@ -1,3 +1,3 @@
 using ObjCRuntime;
 
-[assembly: LinkWith ("libUserVoice.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64, ForceLoad = true)]
+[assembly: LinkWith ("libUserVoice.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64, ForceLoad = true, SmartLink = true)]
